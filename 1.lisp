5268f004a62e973f4cd2ca799b3c40786f16c7d4
Dominic Humphries <dominich@broadbean.com>
2016-10-11 15:39:55 +0100

    Improve first-name

--- 1.lisp
+++ 1.lisp
@@ -1,6 +1,12 @@
+(defparameter *titles*
+  '(Mr Mrs Miss Ms Sir Madam Dr Admiral Major General)
+  "A list of titles that can appear at the start of a name")
+
 (defun first-name (name)
   "Select the first name from a name represented as a list"
-  (first name))
+  (if (member (first name) *titles*)
+    (first-name (rest name))
+    (first name)))
 
 (setf names '((John Q Public) (Malcolm X) (Admiral Grace Murray Hopper)
               (Spot) (Aristotle) (A A Milne) (Z Z Top) (Sir Larry Olivier)
